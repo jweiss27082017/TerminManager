f71aa39804fe4800c1b29fbdcf5a67712c2e56fb
jweiss27082017 <55733858+jweiss27082017@users.noreply.github.com>
2019-10-02 15:06:03 +0200

    Comments von Controller hinzugefügt

--- src/TerminManager/src/main/java/ch/zli/TerminManager/controller/WebController.java
+++ src/TerminManager/src/main/java/ch/zli/TerminManager/controller/WebController.java
@@ -2,21 +2,30 @@ package ch.zli.TerminManager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class WebController {
 
+    //Hier wird die Index Seite zurückgegeben
     @RequestMapping(value = "/index")
     public String index() {
         return "index";
     }
+
+    //Hier wird die Home Seite zurückgegeben
     @RequestMapping(value = "/home")
     public String Home() {
         return "Home";
     }
+
+    //Hier wird die SignIn Seite zurückgegeben
     @RequestMapping(value = "/signin")
     public String SignIn() {
         return "SignIn";
     }
+
+    //Hier wird die SignUp Seite zurückgegeben
     @RequestMapping(value = "/signup")
     public String SignUp() {
         return "SignUp";
